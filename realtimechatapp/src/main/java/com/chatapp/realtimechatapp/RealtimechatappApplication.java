684ae4edc86239e1c59648c99d8299fb28599bd1
vikas-kumar-09 <vikashgupta1234x@gmail.com>
2024-03-12 12:23:25 +0530

    Add initial Files

---
+++ realtimechatapp/src/main/java/com/chatapp/realtimechatapp/RealtimechatappApplication.java
@@ -0,0 +1,14 @@
+package com.chatapp.realtimechatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimechatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimechatappApplication.class, args);
+		System.out.println("Started.........");
+	}
+
+}
---
+++ realtimechatapp/src/test/java/com/chatapp/realtimechatapp/RealtimechatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp.realtimechatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimechatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
